f2f8cd13b846ee4ca5091bad0f2c918a6d9a90ca
G1d00 <jabrmhmd890@gmail.com>
2026-07-17 20:52:24 +0300

    add likes db and functions

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import prisma from "./db";
 
 const app = express();
+const CURRENT_USER_ID = 1;
 app.use(cors());
 app.use(express.json());
 
@@ -10,12 +11,44 @@ app.get("/api/posts", async (req, res) => {
     const limit = Number(req.query.limit) || 20;
     const cursor = req.query.cursor ? Number(req.query.cursor) : undefined;
     const posts = await prisma.post.findMany({
-        include: { author: true },
+        include: {
+            author: true,
+            _count: { select: { likes: true} },
+            likes: { where: { userId: CURRENT_USER_ID }, select: {id: true} },
+        },
         orderBy: { id: "desc" },
         take: limit,
         where: cursor ? { id: { lt:cursor } } : undefined,
     });
     res.json(posts);
+
+    const shaped = posts.map((p) => ({
+        id: p.id,
+        content: p.content,
+        createdAt: p.createdAt,
+        authorId: p.authorId,
+        author: p.author,
+        likeCount: p._count.likes,
+        likedByMe: p.likes.length > 0,
+    }));
+    res.json(shaped);
 });
+
+app.post("/api/posts/:id/like", async (req, res) => {
+    const postId = Number(req.params.id);
+    await prisma.like.createMany({
+        data: [{ userId: CURRENT_USER_ID, postId}],
+        skipDuplicates: true,
+    })
+    res.status(201).json({ok : true});
+});
+
+app.delete("/api/posts/:id/like", async (req,res) => {
+    const postId = Number(req.params.id);
+    await prisma.like.deleteMany({
+        where: { userId: CURRENT_USER_ID, postId}
+    });
+    res.json({ ok: true});
+})
 
 app.listen(3000, () => console.log("listening on http://localhost:3000"));
